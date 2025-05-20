cc18a4433620ee7a3bd3e4543882bdf118307360
RafaDiogoJ <rafabbta@hotmail.com>
2025-05-19 22:31:54 -0300

    Acrescenta carrossel com dados da API

--- rdflix/src/app/services/movie-api.service.ts
+++ rdflix/src/app/services/movie-api.service.ts
@@ -9,6 +9,18 @@ export class MovieApiService {
 
   constructor(private http: HttpClient) { }
 
-  
+  baseUrl = 'https://api.themoviedb.org/3'
+  options ={
+    method: 'GET',
+    headers:{
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmQ5NDE0ZjkzYTY4YTA4YjljNzM3MjQ0ZmNlMDEyZCIsIm5iZiI6MTc0Njc1MzAyMC4yMjcsInN1YiI6IjY4MWQ1NWZjNzNjNmVhZmQyZmIxY2NlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jzK6ERp9StSN06jKvSZ7P5VbVn6vd9SkjLT8bYvylyM'
+    }
+  };
+
+  //Dados para o Banner - Mídias em desaque da semana
+  bannerApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+  }
 
 }
