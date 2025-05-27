ee7d4f32e6a5045fc3e6b5610475ce18209151ae
RafaDiogoJ <rafabbta@hotmail.com>
2025-05-26 22:35:52 -0300

    Implementa carrossel de destques

--- rdflix/src/app/services/movie-api.service.ts
+++ rdflix/src/app/services/movie-api.service.ts
@@ -10,9 +10,9 @@ export class MovieApiService {
   constructor(private http: HttpClient) { }
 
   baseUrl = 'https://api.themoviedb.org/3'
-  options ={
+  options = {
     method: 'GET',
-    headers:{
+    headers: {
       accept: 'application/json',
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNmQ5NDE0ZjkzYTY4YTA4YjljNzM3MjQ0ZmNlMDEyZCIsIm5iZiI6MTc0Njc1MzAyMC4yMjcsInN1YiI6IjY4MWQ1NWZjNzNjNmVhZmQyZmIxY2NlOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jzK6ERp9StSN06jKvSZ7P5VbVn6vd9SkjLT8bYvylyM'
     }
@@ -21,6 +21,21 @@ export class MovieApiService {
   //Dados para o Banner - Mídias em desaque da semana
   bannerApiData(): Observable<any> {
     return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+  }
+
+  // Filmes em destaque do Dia
+  trendingMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/movie/day?language=pt-br`, this.options);
+  }
+
+  // Séries em destaque do dia
+  trendingSerieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/tv/day?language=pt-br`, this.options);
+  }
+
+  // Filmes de Ação Populares
+  popularActionMovieApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/discover/movie?language=pt-br&with_genres=28sort_by=popularity.desc`, this.options);
   }
 
 }
