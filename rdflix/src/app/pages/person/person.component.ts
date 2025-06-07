209db8329ea7c528c88e7e1780fb08f846c968a0
RafaDiogoJ <rafabbta@hotmail.com>
2025-06-06 22:34:56 -0300

    Finaliza a pagina de detalhes da midia e cria pagina do ator

---
+++ rdflix/src/app/pages/person/person.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './person.component.html',
+  styleUrl: './person.component.css'
+})
+export class PersonComponent {
+
+}
